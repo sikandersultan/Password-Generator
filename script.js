b62a0ade390334b2270688ded4f1ae77f332bcc1
sikandersultan <sikandersultanmirza@gmail.com>
2022-01-07 20:56:02 +0000

    .map had wrong variable

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var passwordLength;
+var length;
 var lowerCase;
 var upperCase;
 var numbers;
 var characters;
-var lowercasechar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialchar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", " < ", "=", " > ", " ? ", "@", "[", "/", "]", " ^ ", "_", "`", "{", "|", "}", "~"]
 
@@ -15,9 +15,7 @@ var specialchar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", "
 var toUpper = function (x) {
   return x.toUpperCase();
 };
-upperCasechar = lowerCase.map(toUpper);
-
-
+upperCasechar = lowerCaseChar.map(toUpper);
 
 // Write password to the #password input
 function writePassword() {
@@ -25,8 +23,19 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword () {
+  length = prompt ("How long would you like your password to be? Accepted length is between 8 and 128.")
+  if(length==0){
+  alert("Please enter a value");
+  }
+  else if(length < 8 || length > 128) {
+    lenght = prompt("You must choose between 8 and 128");
+  }
+  else {}
+  
+}
